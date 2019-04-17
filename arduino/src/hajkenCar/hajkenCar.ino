4f8628bc110d0ed711a95786fa08e1e928fcb880
Hartmut Fischer <hartmutfischer@HardyMac.local>
2019-04-17 13:18:11 +0200

    Subject: Add circle driving
    Why: Drive a predetermined circle, circle split into forward and turning
    
    What: Input string array with commands for a circle
    
    Co-authored-by: strand93 <strand.93@hotmail.com>
    
    Fixes #11

--- arduino/src/hajkenCar/hajkenCar.ino
+++ arduino/src/hajkenCar/hajkenCar.ino
@@ -22,7 +22,7 @@ const int gyroOffset = 11;
 //distanceCar
 //**********
 float speed = 50;
-int turningSpeed = 45;
+int turningSpeed = 55;
 int stopSpeed = 0;
 boolean obstacleAvoidanceOn = true; //(de)activate obstacle avoidance for testing
 
@@ -54,7 +54,7 @@ void setup() {
   Serial.begin(9600);
   Serial2.begin(9600); // opens channel for bluetooth, pins 16+17
   Serial2.write("Welcome to HAJKENcar!\nSit back and enjoy the ride.\n "); //Welcome message
-  
+
   //initialize Odometer
   odometer1.attach(ODOMETER1_PIN, []() {
     odometer1.update();
@@ -63,90 +63,67 @@ void setup() {
 
 void loop() {
 
-  testCommands();
-
-  while(true){
-    car.update();
-  }
 
 }
 
 /*
- * METHODS
- */
+   METHODS
+*/
 
-void testCommands(){
-  String commands[] = {"f","100", "r", "45", "f", "70", "b", "150", "r", "15"};
-  int arraySize = sizeof(commands) / sizeof(commands[0]); // 
-  
-  for (int i = 0; i < (arraySize-1); i = i+2) {
-      
-      if(commands[i] == "f") {
-        forward(commands[i+1].toInt());
-      }else if(commands[i] == "b") {
-        backward(commands[i+1].toInt());
-      }else if(commands[i] == "r") {
-        rotate(commands[i+1].toInt());
-      }else {
-        circle();
-      }
+void commands(String commands[], int arraySize) {
+  //int arraySize = sizeof(commands) / sizeof(commands[0]); // does not work with long arrays?!
+  for (int i = 0; i < (arraySize - 1); i = i + 2) {
+
+    if (commands[i] == "f") {
+      forward(commands[i + 1].toInt());
+    } else if (commands[i] == "b") {
+      backward(commands[i + 1].toInt());
+    } else if (commands[i] == "r") {
+      rotate(commands[i + 1].toInt());
+    } else {
+      Serial2.println("unknown or no command");
     }
-}
-
-void circle(){
- 
-  while(car.getDistance()<=370){
-      car.setSpeed(speed);
-      car.update();
-      car.setAngle(45);
-    }
-
-  car.setSpeed(stopSpeed);
-  car.update();
-}
-  
-void rotate(int angleToTurn){
-
-  if(angleToTurn == 0){
-  return; // Dont do anything if angle to turn is 0
   }
-  
+}
+
+void rotate(int angleToTurn) {
+  if (angleToTurn == 0) {
+    return; // Dont do anything if angle to turn is 0
+  }
+  Serial2.println("Car starts to turn");
   int initialHeading = car.getHeading();
   int targetHeading = initialHeading + angleToTurn % 360;
-  
-  if(angleToTurn > 0){
+
+  if (angleToTurn > 0) {
     car.overrideMotorSpeed(turningSpeed, -turningSpeed);
-  }else if(angleToTurn < 0){
+  } else if (angleToTurn < 0) {
     car.overrideMotorSpeed(-turningSpeed, turningSpeed);
   }
 
   Serial2.print("Initial heading: ");
   Serial2.println(initialHeading);
-   
-  int currentTurned = 0;
-  do{
-      Serial2.print("Current heading: ");
-      Serial2.println(car.getHeading());
-      currentTurned = (car.getHeading() - initialHeading) % 360; 
-      if(angleToTurn < 0  && currentTurned > 0){
-        currentTurned = 360 - currentTurned;
-        }
-      Serial2.print("Current turned: ");
-      Serial2.println(currentTurned);
-      car.update();
-    
-  }while (currentTurned<abs(angleToTurn));
 
-  Serial2.println("Car stopped");
+  int currentTurned = 0;
+  do {
+    currentTurned = (car.getHeading() - initialHeading) % 360;
+    if (angleToTurn < 0  && currentTurned > 0) {
+      currentTurned = 360 - currentTurned;
+    }
+
+    car.update();
+
+  } while (currentTurned < abs(angleToTurn));
+  Serial2.print("Totally turned: ");
+  Serial2.println(currentTurned);
   car.setSpeed(stopSpeed);
   car.update();
-  
+
 }
 
 //drives forward up to a set distance
 void forward(int distance) {
   Serial2.write("Going forward\n"); //Printing status
-  
+
   odometer1.reset(); //resets the car's driven distance
   car.setSpeed(speed);
   car.update();
@@ -162,7 +139,7 @@ void forward(int distance) {
 //drives backwards up to a set distance
 void backward(int distance) {
   Serial2.write("Going backward\n "); //Printing status
-  
+
   odometer1.reset(); //resets the car's driven distance
   car.setSpeed(-speed);
   car.update();
@@ -176,23 +153,31 @@ void backward(int distance) {
 }
 
 //drives in a square, starting at lower left corner
-void square(int sideLength){
-for(int i = 1; i<=4; i=i+1){
+void square(int sideLength) {
+  for(int i = 0; i<4; i=i+1){
   forward(sideLength);
+  rotate(90);
   }
+}
+
+void circle() {
+  String testStringCircle[] = {"f","20","r","45","f","20","r","45","f","20","r","45","f","20","r","45","f","20","r","45","f","20","r","45","f","20","r","45","f","20","r","45"};
+  int circleArrayLength = 32;
+  commands(testStringCircle, circleArrayLength);
 }
 
 //obstacle avoidance - stops in front of obstacle + sends message via bluetooth
 void obstacleAvoidance() {
-  if(obstacleAvoidanceOn)
-  {car.update(); 
-  carDistanceToObstacle = USSensorFront.ping_cm(); // UltraSonicSound Sensor measures (0 = more than 100 cm distance)
-  if (carDistanceToObstacle <= stopDistanceToObstacle && carDistanceToObstacle > 0) {
-    Serial2.write("Obstacle detected"); // Sending message to bluetooth
-    car.setSpeed(stopSpeed);
-    car.update();
-    while(true){
-    //Stops car
+  if (obstacleAvoidanceOn)
+  { car.update();
+    carDistanceToObstacle = USSensorFront.ping_cm(); // UltraSonicSound Sensor measures (0 = more than 100 cm distance)
+    if (carDistanceToObstacle <= stopDistanceToObstacle && carDistanceToObstacle > 0) {
+      Serial2.write("Obstacle detected"); // Sending message to bluetooth
+      car.setSpeed(stopSpeed);
+      car.update();
+      while (true) {
+        //Stops car
+      }
+    }
   }
-  }}
 }
